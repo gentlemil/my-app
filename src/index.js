097923dddddbc444e4bd37db52977b5b23d482f6
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2023-01-19 19:36:07 +0100

    try nest components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+// function Greeting() {
+//   return <h2 className='first-component'>my first component</h2>
+// }
+
+// I OPTION
 function Greeting() {
-    return <h2>my first component</h2>;
+  return (
+    <div>
+      <h2>hello world</h2>
+      <ul>
+        <li>
+          <Person />
+        </li>
+      </ul>
+    </div>
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting />);
+// II OPTION
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h2', {}, 'hello world 2')
+//   )
+// }
+
+const Person = () => {
+  return (
+    <div className='myClass'>
+      <p>John Doe</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Greeting />)
