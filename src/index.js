ced434f05f3b93390eadf59e6f83abf0ac0fee3d
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2023-01-19 22:41:46 +0100

    other ways to add styles via JSX

--- src/index.js
+++ src/index.js
@@ -31,7 +31,12 @@ const Image = () => <img src='./images/fairy-tale.jpeg' alt='Fairy Tale' />
 const Title = () => <h2>Fairy Tale</h2>
 
 const Author = () => {
-  return <h4>Stephen King</h4>
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  }
+  return <h4 style={inlineHeadingStyles}>Stephen King</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
