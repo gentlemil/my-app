d3e7b065a24a9bc6b374f7f32d007a1ba42ea5e3
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2023-01-20 17:32:11 +0100

    improve my booklist, write 3 different ways of using objects as a prop and how to map array

--- src/index.js
+++ src/index.js
@@ -5,61 +5,93 @@ import './index.css'
 // Topic: create booklist similar to the UI from Amazon Page (check URL below)
 // https://www.amazon.com/stores/Stephen-King/author/B000AQ0842
 
-const author = 'Stephen King'
-const title = 'Fairy Tale'
-const img = './images/fairy-tale.jpeg'
+const books = [
+  {
+    id: 1,
+    author: 'Stephen King',
+    title: 'Fairy Tale',
+    img: './images/fairy-tale.jpeg',
+  },
+  {
+    id: 2,
+    author: 'J.K.Rowling',
+    title: `Harry Potter and the Philosopher's stone`,
+    img: './images/harry-potter.jpg',
+  },
+  {
+    id: 3,
+    author: 'J.R.R. Tolkien',
+    title: 'The Lord of the Rings The Fellowship of the Ring',
+    img: './images/lord-of-the-ring.jpg',
+  },
+]
 
-const someFunc = (param1, param2) => {
-  console.log(param1, param2)
-}
+// I WAY
+// const BookList = () => {
+//   return (
+//     <section className='booklist'>
+//       {books.map((book, index) => {
+//         const { author, title, img } = book
+//         return <Book img={img} title={title} author={author} key={index} />
+//       })}
+//     </section>
+//   )
+// }
+// const Book = (props) => {
+//   const { id, img, title, author } = props
+//   return (
+//     <article id={id} className='book'>
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   )
+// }
 
-someFunc('job', 'developer')
+// II WAY
+// const BookList = () => {
+//   return (
+//     <section className='booklist'>
+//       {books.map((book, index) => {
+//         const { img, title, author, id } = book
+//         return <Book book={book} key={id} />
+//       })}
+//     </section>
+//   )
+// }
 
+// const Book = (props) => {
+//   const { img, title, author } = props.book
+//   console.log(props)
+//   return (
+//     <article className='book'>
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   )
+// }
+
+// III way
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book author={author} title={title} img={img}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
-          facilis nulla in excepturi reprehenderit accusamus, eveniet voluptatem
-          dolores voluptates reiciendis sunt repellendus sit nisi aspernatur
-          necessitatibus cum? Illo, in voluptates?
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        author='J.K.Rowling'
-        title='Harry Potter and the Philosopher’s Stone'
-        img={img}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props
-  console.log(props)
+  const { img, title, author } = props
   return (
     <article className='book'>
-      <img src={img} alt='Fairy Tale' />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   )
-}
-
-const Image = () => <img src={img} alt='Fairy Tale' />
-
-const Title = () => <h2>Fairy Tale</h2>
-
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '0.5rem',
-  }
-  return <h4 style={inlineHeadingStyles}>Stephen King</h4>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
