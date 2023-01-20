b379b9847b3bf8f03f6a25f11ada12d842623234
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2023-01-20 15:31:35 +0100

    show and try know children props works and consolog it

--- src/index.js
+++ src/index.js
@@ -1,32 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 
 // Topic: create booklist similar to the UI from Amazon Page (check URL below)
 // https://www.amazon.com/stores/Stephen-King/author/B000AQ0842
 
+const author = 'Stephen King'
+const title = 'Fairy Tale'
+const img = './images/fairy-tale.jpeg'
+
+const someFunc = (param1, param2) => {
+  console.log(param1, param2)
+}
+
+someFunc('job', 'developer')
+
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
+          facilis nulla in excepturi reprehenderit accusamus, eveniet voluptatem
+          dolores voluptates reiciendis sunt repellendus sit nisi aspernatur
+          necessitatibus cum? Illo, in voluptates?
+        </p>
+        <button>click me</button>
+      </Book>
+      <Book
+        author='J.K.Rowling'
+        title='Harry Potter and the Philosopher’s Stone'
+        img={img}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author, children } = props
+  console.log(props)
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt='Fairy Tale' />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   )
 }
 
-const Image = () => <img src='./images/fairy-tale.jpeg' alt='Fairy Tale' />
+const Image = () => <img src={img} alt='Fairy Tale' />
 
 const Title = () => <h2>Fairy Tale</h2>
 
